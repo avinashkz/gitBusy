3cc93dceabf13b8945bfaf14a0042206975a8917
ap26793 <zi.avi7@gmail.com>
2018-04-13 13:26:56 -0700

    Changed R requirement

--- tests/testthat/test_repo_count.R
+++ tests/testthat/test_repo_count.R
@@ -3,18 +3,18 @@ context('repo_count.R')
 library(httr)
 library(glue)
 
-#check valid input
-test_that("check if input is in correct format",{
+#check error message
+test_that("check if the error message is correct",{
   expect_error(repo_count("avinashkzz"), 'User avinashkzz Not Found on GitHub',fixed=TRUE)
 })
 
 #check valid output
 test_that('the output has two elements only',{
-  expect_equal(repo_count("avinashkz"),32)
+  expect_equal(repo_count("avinashkz", TRUE),32)
 })
 
 test_that("Check if output is valid",{
-  output <- repo_count("avinashkz")
+  output <- repo_count("sarora", TRUE)
   expect_true(typeof(output)=='integer')
 
 })

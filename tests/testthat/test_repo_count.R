99b52c324d625b0ef8a6a5906a53322372371095
Avinash Prabhakaran <zi.avi7@gmail.com>
2018-06-21 17:23:38 -0700

    Update target repo count

--- tests/testthat/test_repo_count.R
+++ tests/testthat/test_repo_count.R
@@ -15,7 +15,7 @@ test_that("check if the error message is correct",{
 
 #check valid output
 test_that('the output has two elements only',{
-  expect_equal(repo_count("avinashkz", TRUE, token),32)
+  expect_equal(repo_count("avinashkz", TRUE, token),33)
 })
 
 test_that("Check if output is valid",{
